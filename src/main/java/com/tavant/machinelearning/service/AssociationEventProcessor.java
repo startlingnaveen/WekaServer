279cb4821843b48ba6e246899184b80a3950fa52
Naveen <Naveen@NAVEEN-TI9209.local>
2014-11-04 16:12:12 +0530

    Auto turn on AppleTV

--- src/main/java/com/tavant/machinelearning/service/AssociationEventProcessor.java
+++ src/main/java/com/tavant/machinelearning/service/AssociationEventProcessor.java
@@ -1,5 +1,9 @@
 package com.tavant.machinelearning.service;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,16 +28,35 @@ public class AssociationEventProcessor implements EventProcessor {
 			logger.info("Source status: " + rule.getSourceEvent());
 			logger.info("Targe device: " +  rule.getTargetDeviceName());
 			logger.info("Target status: " + rule.getTargetEvent());
+			logger.info("Event name: " + event.getName());
 			logger.info("###################################################");
 			
 			if(event.getName().equalsIgnoreCase(rule.getSourceDeviceName())) {
 				logger.info("Foud rule!!!" );
+		        
+		        sendPost();
 			}
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
+	}
+	
+	private void sendPost() throws Exception {
+		 
+		String url = "http://192.168.2.10:7798/perform/play/Apple%20TV";
+		HttpClient client = HttpClientBuilder.create().build();
+		HttpPost post = new HttpPost(url);
+	 
+		// add header
+		//post.setHeader("User-Agent", USER_AGENT);
+	 
+	 
+		HttpResponse response = client.execute(post);
+		System.out.println("Response Code : " 
+	                + response.getStatusLine().getStatusCode());
+ 
 	}
 
 }
